7bb6c07f2c7ccc81e6e278dfa94e7e6b64311cd3
Shelley Schoppert <schop23@gmail.com>
2018-02-15 14:47:13 -0500

    code notes

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
-
+#the model's name is post and it's properties are title, text, created_date, published_date, and author.
 class Post(models.Model):
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE) #automatically marks the author as the signed in user
+    title = models.CharField(max_length=200) #the title can't be any more than 200 characters long
+    text = models.TextField() #TextField is unlimited text
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
